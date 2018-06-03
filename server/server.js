964fbd17860ed9a2976ce04626203b108fa0d6da
abdelrhman <abdelrhmantalat.93@gmail.com>
2018-06-03 03:02:25 +0200

    add commit and disconnect events

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/..', 'public');
 const PORT = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-  res.render(publicPath + 'index.html');
+io.on('connection', socket => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Started on ${PORT}`);
 });
 
